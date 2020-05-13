a1b1f23c635c37a1b7604df153b311612e27dbb7
lbirts <birtslauren@gmail.com>
2020-05-13 16:18:11 -0500

    fin

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,3 +1,52 @@
 class ApartmentsController < ApplicationController
   # add controller methods
+
+  # set :views, "app/views/apartments"
+
+  get '/apartments' do
+    @apartments = Apartment.all
+    erb :'/apartments/index'
+  end
+
+  get '/apartments/new' do
+    @tenants = Tenant.all
+    erb :'/apartments/new'
+  end
+
+  post '/apartments' do
+    app = Apartment.create(params[:apartment])
+    redirect '/apartments'
+  end
+
+  get '/apartments/:id' do
+    @apartment = current_apartment
+    erb :'/apartments/show'
+  end
+
+  get '/apartments/:id/edit' do
+    @apartment = current_apartment
+    @tenants = Tenant.all
+    erb :'/apartments/edit'
+  end
+  
+  patch '/apartments/:id' do
+    @apartment = current_apartment
+    @apartment.update(params[:apartment])
+    redirect "/apartments/#{@apartment.id}"
+  end
+
+  delete '/apartments/:id' do
+    apartment = current_apartment
+    apartment.destroy
+    redirect '/apartments'
+  end
+
+  post '/apartments/:id' do
+    apartment = current_apartment
+    redirect "/apartments/#{apartment.id}/edit"
+  end
+  
+  def current_apartment
+    @apartment = Apartment.find(params[:id])
+  end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,3 +1,51 @@
 class TenantsController < ApplicationController
   # add controller methods
+  # set :views, "app/views/tenants"
+
+  get '/tenants' do
+    @tenants = Tenant.all
+    erb :'/tenants/index'
+  end
+
+  get '/tenants/new' do
+    @apartments = Apartment.all
+    erb :'/tenants/new'
+  end
+
+  post '/tenants' do
+    tenant = Tenant.create(params)
+    redirect '/tenants'
+  end
+
+  get '/tenants/:id' do
+    @tenant = current_tenant
+    erb :'/tenants/show'
+  end
+
+  get '/tenants/:id/edit' do
+    @tenant = current_tenant
+    @apartments = Apartment.all
+    erb :'tenants/edit'
+  end
+  
+  patch '/tenants/:id' do
+    @tenant = current_tenant
+    @tenant.update(params[:tenant])
+    redirect "/tenants/#{@tenant.id}"
+  end
+
+  delete '/tenants/:id' do
+    tenant = current_tenant
+    tenant.destroy
+    redirect '/tenants'
+  end
+
+  post '/tenants/:id' do
+    tenant = current_tenant
+    redirect "/tenants/#{tenant.id}/edit"
+  end
+  
+  def current_tenant
+    @tenant = Tenant.find(params[:id])
+  end
 end
